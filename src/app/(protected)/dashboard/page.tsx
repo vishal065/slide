2276d6bc101c52baf175d7aeb2cea729be8ec28f
vishal065 <dasvishal60@gmail.com>
2025-01-13 19:00:03 +0530

    basic setup with clerk and db model

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+const Page = async (props: Props) => {
+  //Server action to onBoard the user
+  
+  return <div>Page</div>;
+};
+
+export default Page;
